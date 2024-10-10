6c15cb35dbd9cf3338fe769b610a88a21049796a
RalowiecF <143044384+RalowiecF@users.noreply.github.com>
2024-10-10 00:06:44 -0300

    GastoFuncionandoYGestorDeGastoSinFuncionar (#6)
    * Update .gitignore
    
    * ClaseGastoYComienzoDelGestorDeGastos
    
    Clase gasto funcionando correctamente, gestor de gasto sin funcionar.
    
    * gitIgnore
    
    Actualizacion git ignore
    
    * agrego test y emtidades/servicios
    
    * agrego test nuevos
    
    * test de repos y servicio terminados

---
+++ src/main/java/com/tallerwebi/dominio/GestorDeGastos.java
@@ -0,0 +1,38 @@
+package com.tallerwebi.dominio;
+
+import javax.persistence.*;
+import java.util.*;
+
+@Entity
+public class GestorDeGastos {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL, mappedBy = "gestor", orphanRemoval = true)
+    private List<Gasto> gastos = new ArrayList<Gasto>();
+
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void registrarGasto(Gasto gasto) {
+        this.gastos.add(gasto);
+        gasto.setGestor(this);
+    }
+
+    public void eliminarGasto(Gasto gasto) {
+        this.gastos.remove(gasto);
+        gasto.setGestor(null);
+    }
+
+    public List<Gasto> getGastos() {
+        return gastos;
+    }
+}
---
+++ src/main/java/com/tallerwebi/infraestructura/RepositorioGestorDeGastosImpl.java
@@ -0,0 +1,49 @@
+package com.tallerwebi.infraestructura;
+
+import com.tallerwebi.dominio.Gasto;
+import com.tallerwebi.dominio.GestorDeGastos;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class RepositorioGestorDeGastosImpl {
+
+    private SessionFactory sessionFactory;
+
+    @Autowired
+    public RepositorioGestorDeGastosImpl(SessionFactory sessionFactory){
+        this.sessionFactory = sessionFactory;
+    }
+
+    public List<Gasto> obtenerTodosLosGastosDeUnGestor(Long gestorDeGastosId) {
+        String hql = "SELECT g FROM Gasto g Join GestorDeGastos s ON g.gestor.id = s.id WHERE g.gestor.id = :gestorDeGastosId";
+        Query query = this.sessionFactory.getCurrentSession().createQuery(hql);
+        query.setParameter("gestorDeGastosId", gestorDeGastosId);
+
+        return query.getResultList();
+    }
+
+    public List<GestorDeGastos> obtenerTodosLosGestores() {
+        String hql = "FROM GestorDeGastos";
+        Query query = this.sessionFactory.getCurrentSession().createQuery(hql);
+
+        return query.getResultList();
+    }
+
+    public void guardarGestor(GestorDeGastos gestorDeGastos) {
+        this.sessionFactory.getCurrentSession().save(gestorDeGastos);
+    }
+
+
+    //Para comparar la fecha agendada con la fecha actual
+        /*Date fechaActual = new Date();
+        String fechaVencimiento = "07-10-2024";
+        DateFormat formateadorDeFechas = new SimpleDateFormat("dd-MM-yyyy");
+        Date fechaFormateada = formateadorDeFechas.parse(fechaVencimiento);
+
+        assertThat(fechaActual.before(fechaFormateada), equalTo(true));*/
+}
